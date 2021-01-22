baada6c00c3bd57b5f788a3b6f1a8b6a5d417708
Bogdan Gheorghe <dodebogdan@yahoo.com>
2021-01-22 14:01:17 +0200

    Changed Rectangle and Square

--- FSD OOP/FSD OOP/Square.cs
+++ FSD OOP/FSD OOP/Square.cs
@@ -8,7 +8,21 @@ namespace FSD_OOP
     {
         public Square(double height, Color color) : base(height, height, color)
         {
+        }
 
+        public new void Draw()
+        {
+
+            String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Console.Write("The square has it's corners at: ");
+
+            for (int index = 0; index < NumberOfAngles; index++)
+            {
+                Console.Write($" {alphabet[index]}({Position[index].X},{Position[index].Y}), ");
+            }
+
+            Console.WriteLine($"The color of the rectangle is: {Color}");
         }
 
     }
